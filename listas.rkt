b3ca85dc8632d9791a09d1bb89628a0fb7e2574c
Maria Jesus Canoles <maria.canoles.r@usach.cl>
2020-05-15 01:46:57 -0400

    Tda local repository terminado, falta comentar el codigo. Además se agregaron funciones al archivo de listas

--- listas.rkt
+++ listas.rkt
@@ -13,6 +13,7 @@
                                                                       (calcular-largo-lista-aux (cdr L) (+ n 1)))))
                                (calcular-largo-lista-aux L 0)))
 
+
 ;Descripción: Funcion que aplica una funcion a un elemento
 ;Dominio: Función y elemento que puede ser un string o entero
 ;Recorrido: Booleano
@@ -21,6 +22,7 @@
                                    (if (f elemento)
                                        #t
                                        #f)))
+
 
 ;Descripción: Función que aplica una función a todos los elementos de una lista
 ;Dominio: Función, entero representando el largo de lista y una lista
@@ -33,12 +35,27 @@
                                         (aplicar-funcion-lista f (- n 1) (cdr L))
                                         #f))))
 
+
+;Descripción: Función que aplica una función a una lista de listas
+;Dominio: Función, entero representando el largo de lista y una lista de listas
+;Recorrido: Booleano
+;Recursión: Cola
+(define aplicar-funcion-lista-de-listas (lambda (f n L)
+                                          (if (null? L)
+                                              #t
+                                              (if (aplicar-funcion-lista f (calcular-largo-lista (car L)) (car L))
+                                                  (aplicar-funcion-lista-de-listas f (- n 1) (cdr L))
+                                                  #f))))
+                                          
+
+
 ;Descripción: Función que agrega un elemento al final de una lista
 ;Dominio: Elemento que puede ser un string o entero y una lista
 ;Recorrido: Lista con nuevo elemento
 ;Recursión:
 (define agregar-elemento-final-lista (lambda (elemento L)
                                        (append L (list elemento))))
+
 
 ;Descripción: Función que agrega una lista aux a una lista
 ;Dominio: Dos listas
@@ -51,6 +68,7 @@
                                                                                 (agregar-lista-final-lista-aux (- largo 1) (cdr lista) (agregar-elemento-final-lista (car lista) L)))))
                                     (agregar-lista-final-lista-aux (calcular-largo-lista lista) lista L)))
 
+
 ;Descripción: Función que comprueba si un elemento se encuentra dentro de una lista
 ;Dominio: String o entero representando el elemento y una lista 
 ;Recorrido: Booleano
@@ -61,7 +79,16 @@
                                 (if (null? (cdr L))
                                     #f
                                     (elemento-en-lista elemento (cdr L))))))
-                                
+
+
+;Descripción: Función que obtiene un elemento de una lista
+;Dominio: Entero n y Lista L
+;Recorrido: Elemento de la lista
+;Recursión: Cola
+(define obtener-elemento-lista (lambda (n L)
+                                 (if (= n 0)
+                                     (car L)
+                                     (obtener-elemento-lista (- n 1) (cdr L)))))
                              
 
 
--- tda_3.rkt
+++ tda_3.rkt
@@ -9,14 +9,16 @@
 
 ;-----<CONSTRUCTOR>-----
 
-;Descripción: 
+;Descripción: Función que 
 ;Dominio: 
 ;Recorrido: 
 ;Recursión:
-(define (local-repository . commit)
-  (if (> (calcular-largo-lista commit) 0)
-      (if (aplicar-funcion-lista string? (calcular-largo-lista commit) commit)
-          commit
+(define (local-repository rama . commit)
+  (if (string? rama)
+      (if (> (calcular-largo-lista commit) 1)
+          (if (aplicar-funcion-lista string? (calcular-largo-lista commit) commit)
+              (cons rama (list commit))
+              '())
           '())
       '()))
 
@@ -26,6 +28,18 @@
 ;Dominio: 
 ;Recorrido: 
 ;Recursión:
+(define local-repository? (lambda (L)
+                            (if (list? L)
+                               (if (null? L)
+                                   #f
+                                   (if (> (calcular-largo-lista (cdr L)) 1)
+                                       (if (aplicar-funcion-lista-de-listas string? (calcular-largo-lista (cdr L)) (cdr L))
+                                           #t
+                                           #f)
+                                       #f))
+                               #f)))
+                                       
+                           
 
 ;-----<SELECTORES>-----
 
@@ -33,6 +47,13 @@
 ;Dominio: 
 ;Recorrido: 
 ;Recursión:
+(define get-commit-local-repository (lambda (n L)
+                                      (if (local-repository? L)
+                                          (if (< n (calcular-largo-lista L))
+                                              (obtener-elemento-lista n (cdr L))
+                                              -1)
+                                          -1)))
+
 
 ;-----<MODIFICADORES>-----
 
@@ -41,3 +62,17 @@
 ;Recorrido: 
 ;Recursión:
 
+(define agregar-commit-local-repository (lambda (commit L)
+                                          (define agregar-commit-local-repository-aux (lambda (commit L nuevo-L)
+                                                                                        (if (null? L)
+                                                                                            (agregar-elemento-final-lista commit nuevo-L)
+                                                                                            (agregar-commit-local-repository-aux commit (cdr L) (agregar-elemento-final-lista (car L) nuevo-L)))))
+                                          (if (local-repository? L)
+                                              (if (> (calcular-largo-lista commit) 1)
+                                                  (if (aplicar-funcion-lista string? (calcular-largo-lista commit) commit)
+                                                      (agregar-commit-local-repository-aux commit L '())
+                                                      -1)
+                                                  -1)
+                                              -1)))
+                                              
+                                                                                                                        
